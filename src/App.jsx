b1ee75ad46ebda1bafbfe55e9bab0578dcebb47d
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2025-07-09 19:55:36 +0530

    Implemented XDictionary App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-// import { useState } from 'react'
+import XDictionary from "./XDictionary";
 
 function App() {
 
   return (
     <>
-      <h1>XDICTIONARY</h1>
+      <XDictionary/>
     </>
   )
 }
